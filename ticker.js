4f3ff052d6d83728259ace627f3a07b672c60219
PCMUSA <pluscareusa@gmail.com>
2021-12-15 02:50:28 -0700

    Ajax可以跨域请求查询shopify店铺的graphQL API，但是mutation更新同时传递变量的情况不能成功解析

---
+++ ticker.js
@@ -0,0 +1,5 @@
+var data = {symbol:'IBM', price:100};
+showPrice(data);
+var p = document.createElement("p");
+document.body.appendChild(p);
+document.getElementsByClassName("text")[0].innerHTML = '';
